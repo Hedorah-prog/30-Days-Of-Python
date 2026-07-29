fdd6e3475d0a1fc0a2a1f172b88a6426b54506fc
Hedorah-prog <lebryanbellevue@gmail.com>
2026-07-29 16:00:56 -0400

    Fix boolean value and add new variables

---
+++ 02_Day_Variables_builtin_functions/variables/.py
@@ -0,0 +1,14 @@
+# Keeps the directory in git (placeholder file)
+#Day 2: 30 Days of python programming
+first_name = Bryan 
+last_name = Bellevue
+full_name = Bryan Bellevue
+country = canada
+city = Bois-des-Fillions
+age = 19
+year = 2026
+is_married = False
+is_true = 'likes apples'
+is_light_on = True 
+current_ennemies = ['Nouman , Sébastien']
+print(year)
